f4a5ae79e30dd7618c6e3798cb8086b5852c6e7b
Diogo Silva <digax96@gmail.com>
2019-03-20 10:32:08 +0000

    Peer now creates folder to save chunks

--- src/peer/Peer.java
+++ src/peer/Peer.java
@@ -1,6 +1,28 @@
 package peer;
 
+import java.io.File;
+
 public class Peer {
-	
-	
+	private int peerId;
+
+	public Peer(int peerId) {
+		this.peerId = peerId;
+		createDir();
+	}
+
+	public static void main(String[] args) {
+		int id = Integer.parseInt(args[0]);
+		Peer peer = new Peer(id);
+	}
+
+	public boolean createDir() {
+		File dir = new File("./peers/peer" + peerId);
+		boolean success = dir.mkdirs();
+		/*if (!success) {
+			System.out.println("Failed to create peer's directory");
+		} else {
+			System.out.println("Created peer's directory");
+		}*/
+		return success;
+	}
 }
