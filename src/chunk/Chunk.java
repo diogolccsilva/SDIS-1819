9f0ee7dd06e62040ede78d41e057657bb93d669f
Diogo Silva <digax96@gmail.com>
2019-04-06 20:43:46 +0100

    Added static function to split file into chunks

--- src/chunk/Chunk.java
+++ src/chunk/Chunk.java
@@ -1,5 +1,9 @@
 package chunk;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
 public class Chunk {
     public static final int CHUNK_MAX_SIZE = 64000;
 
@@ -17,19 +21,47 @@ public class Chunk {
         this.repDegree = repDegree;
     }
 
+    /**
+     * @return the chunk's file ID
+     */
     public String getFileID() {
         return fileID;
     }
 
+    /**
+     * @return the chunk number
+     */
     public int getChunkNo() {
         return chunkNo;
     }
 
+    /**
+     * @return the chunk replication degree
+     */
     public int getRepDegree() {
         return repDegree;
     }
 
+    /**
+     * @return the chunk data byte array
+     */
     public byte[] getData() {
         return data;
     }
+
+    public static Chunk[] splitFile(File file, int repDegree) {
+        int nChunks = (int) (file.getTotalSpace() / CHUNK_MAX_SIZE);
+        Chunk[] chunks = new Chunk[nChunks];
+        try (RandomAccessFile data = new RandomAccessFile(file, "r")) {
+            byte[] eight = new byte[8];
+            for (int i = 0; i < nChunks; i++) {
+                data.readFully(CHUNK_MAX_SIZE);
+                // do something with the 8 bytes
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return chunks;
+    }
+
 }
