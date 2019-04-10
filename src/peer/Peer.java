2678f66a249dc031e05d1511838415a3b4679e95
unknown <zemafaz@gmail.com>
2019-04-10 16:44:50 +0100

    Registering in the Registry

--- src/peer/Peer.java
+++ src/peer/Peer.java
@@ -4,10 +4,14 @@ import disk.*;
 import peer.channels.*;
 
 import java.io.IOException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.lang.Thread;
 
-public class Peer implements PeerInterface{
+public class Peer extends UnicastRemoteObject implements PeerInterface{
 	private int peerId;
 	private Disk disk;
 
@@ -26,10 +30,12 @@ public class Peer implements PeerInterface{
 
 
 	public Peer(String pVersion, int sid, String accessPoint, String mcAddress, int mcPort, String mdbAddress,
-			int mdbPort, String mdrAddress, int mdrPort) {
+			int mdbPort, String mdrAddress, int mdrPort) throws RemoteException {
+		super();
 		this.peerId = sid;
 		this.disk = new Disk("peer" + peerId);
 		this.pVersion = pVersion;
+		this.rmiReg = null;
 
 		this.startRMI();
 		
@@ -48,11 +54,22 @@ public class Peer implements PeerInterface{
 		int mcPort = Integer.parseInt(args[4]);
 		int mdbPort = Integer.parseInt(args[6]);
 		int mdrPort = Integer.parseInt(args[8]);
-		Peer peer = new Peer(args[0], id, args[2], args[3], mcPort, args[5], mdbPort, args[7], mdrPort);
+		try {
+			Peer peer = new Peer(args[0], id, args[2], args[3], mcPort, args[5], mdbPort, args[7], mdrPort);
+		} catch(RemoteException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void startRMI(){
-
+		try {
+			this.rmiReg = LocateRegistry.getRegistry();
+			rmiReg.bind(Integer.toString(this.peerId), this);
+		} catch(RemoteException e) {
+			e.printStackTrace();
+		} catch(AlreadyBoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void startChannels(String mcA, int mcP, String mdbA, int mdbP, String mdrA, int mdrP) throws IOException{
