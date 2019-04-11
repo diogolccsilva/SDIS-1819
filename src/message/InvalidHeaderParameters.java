881885d42901cffd2af54f79c48ec3071051dad6
Diogo Silva <digax96@gmail.com>
2019-04-11 13:10:29 +0100

    Create InvalidHeaderParameters.java

---
+++ src/message/InvalidHeaderParameters.java
@@ -0,0 +1,11 @@
+package message;
+
+/**
+ * InvalidHeaderParameters
+ */
+public class InvalidHeaderParameters extends Exception {
+
+	private static final long serialVersionUID = -1129574923771068119L;
+
+		
+}
