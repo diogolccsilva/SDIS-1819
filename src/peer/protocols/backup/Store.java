42995a17a0cd5e65842585b91fba35a6346706cd
Diogo Silva <digax96@gmail.com>
2019-04-11 13:11:27 +0100

    Update Store.java

--- src/peer/protocols/backup/Store.java
+++ src/peer/protocols/backup/Store.java
@@ -1,9 +1,37 @@
 package peer.protocols.backup;
 
+import chunk.Chunk;
+import message.Message;
+import peer.Peer;
+
 /**
  * Store
  */
-public class Store {
+public class Store implements Runnable {
 
-	
+	private Peer peer;
+	private Chunk chunk;
+
+	public Store(Peer peer, Chunk chunk) {
+		this.peer = peer;
+		this.chunk = chunk;
+	}
+
+	public void sendStored() {
+		Message storedMessage = Message.parseStoredMessage(chunk, peer.getPeerId());
+	}
+
+	public boolean storeChunk() {
+		if (peer.getDisk().storeChunk(chunk)) {
+			sendStored();
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void run() {
+		storeChunk();
+	}
+
 }
