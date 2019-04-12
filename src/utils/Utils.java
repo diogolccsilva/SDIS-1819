dc30163125a7aecfb5fd732326eb36f8d26fb2bb
Diogo Silva <digax96@gmail.com>
2019-04-12 12:07:28 +0100

    Added function to delete directories recursively

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Utils
@@ -8,13 +9,17 @@ import java.io.File;
 public class Utils {
 
     public static boolean isInteger(String s) {
-        if(s.isEmpty()) return false;
-        for(int i = 0; i < s.length(); i++) {
-            if(i == 0 && s.charAt(i) == '-') {
-                if(s.length() == 1) return false;
-                else continue;
+        if (s.isEmpty())
+            return false;
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 0 && s.charAt(i) == '-') {
+                if (s.length() == 1)
+                    return false;
+                else
+                    continue;
             }
-            if(Character.digit(s.charAt(i), 10) < 0) return false;
+            if (Character.digit(s.charAt(i), 10) < 0)
+                return false;
         }
         return true;
     }
@@ -23,7 +28,21 @@ public class Utils {
         return new File(filePath).isFile();
     }
 
-    public static boolean isFloat(String s){
+    public static boolean isFloat(String s) {
         return s.matches("[0-9]*\\.?[0-9]+");
+    }
+
+    public static void deleteDirectoryRecursively(File file) throws IOException {
+        if (file.isDirectory()) {
+            File[] entries = file.listFiles();
+            if (entries != null) {
+                for (File entry : entries) {
+                    deleteDirectoryRecursively(entry);
+                }
+            }
+        }
+        if (!file.delete()) {
+            throw new IOException("Failed to delete " + file);
+        }
     }
 }
