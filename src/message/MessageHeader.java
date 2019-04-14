31e8511e1bf02bb1f53a3e197fe81e58f652337d
Diogo Silva <digax96@gmail.com>
2019-04-14 22:24:47 +0100

    Minor changes no Message

--- src/message/MessageHeader.java
+++ src/message/MessageHeader.java
@@ -2,6 +2,7 @@ package message;
 
 public class MessageHeader {
 
+    public static final String CRLF = "\r\n\r\n";
     public static final int MESSAGE_HEADER_MAX_SIZE = 1000;
 
     private String messageType, version, fileId;
@@ -88,26 +89,22 @@ public class MessageHeader {
         switch (messageType) {
         case "GETCHUNK":
             return this.messageType + " " + this.version + " " + Integer.toString(this.senderId) + " " + this.fileId
-                    + " " + Integer.toString(this.chunkNo) + "\r\n\r\n";
+                    + " " + Integer.toString(this.chunkNo) + CRLF;
         case "PUTCHUNK":
             return this.messageType + " " + this.version + " " + Integer.toString(this.senderId) + " " + this.fileId
-                    + " " + Integer.toString(this.chunkNo) + " " + Integer.toString(this.replicaDeg) + "\r\n\r\n";
+                    + " " + Integer.toString(this.chunkNo) + " " + Integer.toString(this.replicaDeg) + CRLF;
         case "STORED":
             return this.messageType + " " + this.version + " " + Integer.toString(this.senderId) + " " + this.fileId
-                    + " " + Integer.toString(this.chunkNo) + "\r\n\r\n";
+                    + " " + Integer.toString(this.chunkNo) + CRLF;
         case "CHUNK":
             return this.messageType + " " + this.version + " " + Integer.toString(this.senderId) + " " + this.fileId
-                    + " " + Integer.toString(this.chunkNo) + "\r\n\r\n";
+                    + " " + Integer.toString(this.chunkNo) + CRLF;
         case "DELETE":
             return this.messageType + " " + this.version + " " + Integer.toString(this.senderId) + " " + this.fileId
-                    + "\r\n\r\n";
+                    + CRLF;
         default:
             return "";
 
         }
-        // return this.messageType + " " + this.version + " " +
-        // Integer.toString(this.senderId) + " " + this.fileId + " " +
-        // Integer.toString(this.chunkNo) + " " +
-        // Integer.toString(this.replicaDeg)+"\r\n\r\n";
     }
 }
