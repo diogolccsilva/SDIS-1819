c67fee0014801012b4b61a33f1b0392b584bd6ac
Diogo Silva <digax96@gmail.com>
2020-04-14 18:06:22 +0100

    Added run bat files

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -cp .\bin peer.Peer 1 1 accesspoint "224.0.0.2" 50 "224.0.0.3" 50 "224.0.0.4" 50
+java -cp .\bin peer.Peer 1 1 accesspoint "224.0.0.2" 3434 "224.0.0.3" 3434 "224.0.0.4" 3434
---
+++ runSnooper.bat
@@ -0,0 +1,1 @@
+java -jar .\McastSnooper.jar logfile.txt 224.0.0.2:50 224.0.0.3:50 224.0.0.4:50
